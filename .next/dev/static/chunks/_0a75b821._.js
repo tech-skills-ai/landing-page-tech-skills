0b6802d3a26b42bfe1ffeeb063c830c2796258f7
Gustavo de Sousa Lima <gustavdesousalima@gmail.com>
2026-02-22 03:00:43 -0300

    Enhance deployment configuration by adding NEXT_PUBLIC_BASE_PATH environment variable and update video source path to use dynamic base path. Add new CSS classes for layout adjustments.

--- .next/dev/static/chunks/_0a75b821._.js
+++ .next/dev/static/chunks/_0a75b821._.js
@@ -239,6 +239,7 @@ __turbopack_context__.s([
     "VideoSection",
     ()=>VideoSection
 ]);
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$build$2f$polyfills$2f$process$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = /*#__PURE__*/ __turbopack_context__.i("[project]/node_modules/next/dist/build/polyfills/process.js [app-client] (ecmascript)");
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/compiled/react/jsx-dev-runtime.js [app-client] (ecmascript)");
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$index$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/compiled/react/index.js [app-client] (ecmascript)");
 ;
@@ -297,7 +298,7 @@ function VideoSection() {
                         className: "relative aspect-video overflow-hidden bg-transparent",
                         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("video", {
                             ref: videoRef,
-                            src: "/demo-tech-skills.mp4",
+                            src: `${__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$build$2f$polyfills$2f$process$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"].env.NEXT_PUBLIC_BASE_PATH ?? ""}/demo-tech-skills.mp4`,
                             autoPlay: true,
                             loop: true,
                             muted: true,
